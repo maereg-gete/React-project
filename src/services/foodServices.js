d5336cf8b572892a4de8a9b452ee4465344b8116
maereg-gete <140716223+maereg-gete@users.noreply.github.com>
2023-08-25 17:14:29 +0300

    Delete src directory

--- src/services/foodServices.js
+++
@@ -1,8 +0,0 @@
-import { sample_foods } from "../data";
-
-export const getAll = async () => sample_foods;
-
-export const search = async searchTerm => 
-    sample_foods.filter(item => 
-        item.name.toLowerCase().includes(searchTerm.toLowerCase())
-        );
