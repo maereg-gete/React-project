e56efd86fd875fe882b0c2a8a3646e80d242e121
maereg-gete <140716223+maereg-gete@users.noreply.github.com>
2023-08-25 17:18:42 +0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import Header from "./components/Header/Header";
+import AppRoutes from './AppRoutes';
+
+function App() {
+  return <>
+  <Header />
+  <AppRoutes />
+  </>;
+}
+
+export default App;
